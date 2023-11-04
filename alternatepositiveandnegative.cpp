378e6a9a18103479e25889662e8c7c6bf621d859
TitusShachinSMR <143421175+TitusShachinSMR@users.noreply.github.com>
2023-11-03 23:40:59 -0700

    Add files via upload

---
+++ alternatepositiveandnegative.cpp
@@ -0,0 +1,32 @@
+/*Given an array having positive and negative numbers, our task is to arrange them in an alternate fashion such that every positive number is followed by a negative number and vice-versa maintaining the order of appearance. The number of positive and negative numbers need not to be equal. If there are more positive numbers then they have to appear at the end of the array , same condition for negative numbers also .*/
+#include<stdio.h>
+int main()
+{int n,i,j,temp,l=0;
+printf("enter the lenght of array\n");
+scanf("%d",&n);int a[n];
+
+printf("enter the array:"); 
+for(i=0;i<n;i++)
+{scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{if(a[i]<0){l=l+1;}
+}
+int t=0,p=0;int b[l],c[n-l];
+for(i=0;i<n;i++)
+{
+{if(a[i]<0){b[t]=a[i];++t;}
+}
+if(a[i]>0){c[p]=a[i];++p;}
+} t=0;p=0;
+for(i=0;i<n;i++)
+{ if(i%2==1&&t<=l-1){a[i]=b[t];++t;}
+if(i%2==0&&p<=n-l-1){a[i]=c[p];++p;
+}
+}
+for(i=0;i<n;i++)
+{printf("%d ",a[i]);
+}
+}
+
+
