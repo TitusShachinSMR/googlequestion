c87e7f6c1fdf2d9486b78e5e456d9dbb71b689f5
TitusShachinSMR <143421175+TitusShachinSMR@users.noreply.github.com>
2023-11-04 06:42:51 -0700

    Add files via upload

---
+++ lowestlostpositiveinteger.cpp
@@ -0,0 +1,37 @@
+//minimum postitve number missing in array 
+//having positive and negative number
+#include<stdio.h>
+int main()
+{ int n,i,j,l=0,m,temp;
+ printf("enter the array size\n");
+ scanf("%d",&n);int a[n];
+ printf("enter array\n");
+ for(i=0;i<n;i++)
+ {scanf("%d",&a[i]);
+ }
+ for(i=0;i<n;i++)
+ {if(a[i]>0){l=l+1;
+ }
+ }int b[l];int t=0;
+ for(i=0;i<n;i++)
+ { if(a[i]>=0)
+ {b[t]=a[i];t++;
+ }
+ }
+ for(i=0;i<l-1;i++)
+ {for(j=0;j<l-1-1;j++)
+ { if(b[i]>b[j])
+ {temp=b[i];
+ b[i]=b[j];
+ b[j]=temp;
+ }
+ }
+ }
+int min=b[0];
+ for(i=0;i<l;i++)
+ { 
+ if(b[i]!=(min+i))
+ {m=i;break;
+ }
+ }
+ printf("missed positive smallest integer =%d",b[m]-1);}
